a9d8abc5c67ff376bbf4ec18f9516424b857f40f
Winson-030 <100707097+Winson-030@users.noreply.github.com>
2022-03-02 16:40:12 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,12 +1,9 @@
 
-document.addEventListener("DOMContentLoaded"
-, function(event){
-    
-let targetImage = document.querySelector("#smart-image");
-targetImage.addEventListener("click" , function(){
-   
-    targetImage.classList.remove("small");targetImage.classList.add("small");
 
+document.addEventListener("DOMContentLoaded", function(event) {
+    // 你的所有代码都应该写在这里
+    let targetImage = document.querySelector("#smart-image");
+    targetImage.addEventListener("click", function() {
+    alert("点击了图片");
 });
-
 });
